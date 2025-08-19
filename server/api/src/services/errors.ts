d341eb5bfea8dde72af65e5e5ee18c873b92a359
ezemastro <mastropietroezequiel@gmail.com>
2025-08-19 19:21:53 -0300

    Creación de errores

---
+++ server/api/src/services/errors.ts
@@ -0,0 +1,20 @@
+export class InvalidInputError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "InvalidInputError";
+  }
+}
+
+export class InternalServerError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "InternalServerError";
+  }
+}
+
+export class ConflictError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ConflictError";
+  }
+}
