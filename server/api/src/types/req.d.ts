48c912e60721ded289056823f47383047899d399
ezemastro <mastropietroezequiel@gmail.com>
2025-08-21 11:29:03 -0300

    Agregando las cookies y la session al tipo Request

---
+++ server/api/src/types/req.d.ts
@@ -0,0 +1,14 @@
+import "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      cookies: {
+        [key: string]: string;
+      };
+      session?: {
+        userId: string;
+      };
+    }
+  }
+}
