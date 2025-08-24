38bfb8dd4ea14da97a447760eadc078786c2b16d
ezemastro <mastropietroezequiel@gmail.com>
2025-08-24 19:06:53 -0300

    Separación en proyectos para tener test tanto unitarios como end-to-end

--- server/api/jest.config.js
+++ server/api/jest.config.js
@@ -1,12 +1,31 @@
 export default {
-  preset: "ts-jest",
   testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  setupFiles: ["<rootDir>/src/config.ts"],
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1", // le saca el .js en tests
-  },
-  // globalSetup: "<rootDir>/src/tests/setup.ts",
-  globalTeardown: "<rootDir>/src/tests/teardown.ts",
-  setupFilesAfterEnv: ["<rootDir>/src/tests/setupAfterEnv.ts"],
+  projects: [
+    {
+      moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1", // le saca el .js en tests
+      },
+      roots: ["<rootDir>/src"],
+      preset: "ts-jest",
+      displayName: "api",
+      testMatch: ["**/tests/**/*.test.ts"],
+      globalTeardown: "./src/tests/teardown.ts",
+      setupFilesAfterEnv: ["./src/tests/setupAfterEnv.ts"],
+    },
+    {
+      moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1", // le saca el .js en tests
+      },
+      roots: ["<rootDir>/src"],
+      preset: "ts-jest",
+      displayName: "unit",
+      testMatch: [
+        "**/models/**/*.test.ts",
+        "**/controllers/**/*.test.ts",
+        "**/routes/**/*.test.ts",
+        "**/utils/**/*.test.ts",
+        "**/services/**/*.test.ts",
+      ],
+    },
+  ],
 };
