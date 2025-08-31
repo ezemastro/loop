65e565406453fa1d5f696d72098c8ce87495f6a3
ezemastro <mastropietroezequiel@gmail.com>
2025-08-31 16:54:42 -0300

    Actualización de configuraciones y dependencias: se modificó el archivo app.json para incluir el paquete, se actualizaron las dependencias en package.json y package-lock.json, y se mejoró la configuración de tailwind.config.js añadiendo colores personalizados.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"];
+export const content = [
+  "./app/**/*.{js,jsx,ts,tsx}",
+  "./components/**/*.{js,jsx,ts,tsx}",
+];
 export const presets = [require("nativewind/preset")];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      primary: "#FF5900",
+      secondary: "#4C9F38",
+      tertiary: "#009E7C",
+      "main-text": "#424242",
+      "secondary-text": "#9E9E9E",
+      credits: "#FFA826",
+      stroke: "#E4E4E4",
+      background: "#EAEAEA",
+    },
+  },
 };
 export const plugins = [];
