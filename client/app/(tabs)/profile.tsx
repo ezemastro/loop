90bbceda627e687566d75b3a489902c77ca0c595
ezemastro <mastropietroezequiel@gmail.com>
2025-09-05 12:31:31 -0300

    Agregar función de cierre de sesión y corregir texto en la página de perfil

--- client/app/(tabs)/profile.tsx
+++ client/app/(tabs)/profile.tsx
@@ -1,9 +1,12 @@
+import { useSessionStore } from "@/stores/session";
 import { View, Text } from "react-native";
 
 export default function ProfilePage() {
+  const { logout } = useSessionStore();
+  logout();
   return (
     <View>
-      <Text>P</Text>
+      <Text>Profile</Text>
     </View>
   );
 }
