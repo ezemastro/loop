050bf7aa9f913ac424cf2535632e6491357e82f7
ezemastro <mastropietroezequiel@gmail.com>
2025-09-16 14:27:39 -0300

    Agregar componente Stats para mostrar estadísticas de reciclaje y ahorro de recursos

---
+++ client/components/Stats.tsx
@@ -0,0 +1,25 @@
+import { View } from "react-native";
+import Stat from "./cards/Stat";
+import { CO2Icon, H20Icon, WasteIcon } from "./Icons";
+
+export default function Stats() {
+  return (
+    <View className="flex-row justify-between px-4 my-4 gap-2">
+      <Stat
+        label="Kg. de desechos reciclados"
+        value={150}
+        icon={<WasteIcon className="text-primary" size={42} />}
+      />
+      <Stat
+        label="Kg. de CO2 ahorrado"
+        value={350}
+        icon={<CO2Icon className="text-secondary" size={42} />}
+      />
+      <Stat
+        label="Litros de H20 ahorrados"
+        value={24}
+        icon={<H20Icon className="text-tertiary" size={42} />}
+      />
+    </View>
+  );
+}
