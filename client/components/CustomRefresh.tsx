6c776c6fcd6ef9a4ed0a1bc71f3f54149ac8c9d2
ezemastro <mastropietroezequiel@gmail.com>
2025-09-16 14:27:18 -0300

    Agregar componente CustomRefresh y habilitar refresco en MainView

---
+++ client/components/CustomRefresh.tsx
@@ -0,0 +1,17 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { RefreshControl, RefreshControlProps } from "react-native";
+
+export default function CustomRefresh(
+  props: Partial<
+    React.ReactElement<
+      RefreshControlProps,
+      string | React.JSXElementConstructor<any>
+    >
+  >,
+) {
+  const queryClient = useQueryClient();
+  const onRefresh = async () => {
+    await queryClient.invalidateQueries();
+  };
+  return <RefreshControl refreshing={false} onRefresh={onRefresh} {...props} />;
+}
--- client/components/bases/MainView.tsx
+++ client/components/bases/MainView.tsx
@@ -1,12 +1,22 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { twMerge } from "tailwind-merge";
+import CustomRefresh from "../CustomRefresh";
 
 export const MainView = ({
   children,
   className,
+  refreshEnabled,
 }: {
   children: React.ReactNode;
   className?: string;
+  refreshEnabled?: boolean;
 }) => {
-  return <View className={twMerge(`flex-1`, className)}>{children}</View>;
+  if (refreshEnabled) {
+    return (
+      <View className={twMerge("flex-1", className)}>
+        <ScrollView refreshControl={<CustomRefresh />}>{children}</ScrollView>
+      </View>
+    );
+  }
+  return <View className={twMerge("flex-1", className)}>{children}</View>;
 };
