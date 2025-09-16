47ccf99fc556e328fe3364b86dc3c061e86476a5
ezemastro <mastropietroezequiel@gmail.com>
2025-09-16 12:08:22 -0300

    Agregar componente ListingViewList para mostrar publicaciones con carga y manejo de errores

---
+++ client/components/bases/ListingViewList.tsx
@@ -0,0 +1,35 @@
+import { View, Text, Pressable } from "react-native";
+import Listing from "../cards/Listing";
+import Loader from "../Loader";
+import Error from "../Error";
+
+export default function ListingViewList({
+  listings,
+  isLoading,
+  isError,
+  hasNextPage,
+  fetchNextPage,
+}: {
+  listings: Listing[];
+  isLoading: boolean;
+  isError: boolean;
+  hasNextPage: boolean;
+  fetchNextPage: () => void;
+}) {
+  return (
+    <View>
+      <View className="gap-2">
+        {listings.map((listing) => (
+          <Listing key={listing.id} listing={listing} />
+        ))}
+        {isLoading && <Loader />}
+        {isError && <Error>Error al cargar las publicaciones</Error>}
+      </View>
+      {hasNextPage && !isLoading && (
+        <Pressable className="p-4 items-center" onPress={() => fetchNextPage()}>
+          <Text>Cargar más</Text>
+        </Pressable>
+      )}
+    </View>
+  );
+}
