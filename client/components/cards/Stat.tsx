17d252bda2fb8882cbaeda82265b4f7d0231f172
ezemastro <mastropietroezequiel@gmail.com>
2025-09-16 15:42:03 -0300

    Actualizar estilos en componentes Stats y Stat para mejorar la presentación visual

--- client/components/cards/Stat.tsx
+++ client/components/cards/Stat.tsx
@@ -15,13 +15,13 @@ export default function Stat({
   return (
     <View
       className={twMerge(
-        "px-2 py-4 gap-1 border border-stroke rounded-lg items-center flex-1 max-w-[31%]",
+        "px-2 py-4 gap-1 bg-white border border-stroke rounded-lg items-center flex-1 max-w-[31%]",
         className,
       )}
     >
       {icon}
       <Text className="text-2xl text-main-text font-bold mt-2">{value}</Text>
-      <View className="items-center flex-1 justify-center">
+      <View className="items-center justify-center flex-grow">
         <Text className="text-main-text/90 text-sm text-center font-normal">
           {label}
         </Text>
