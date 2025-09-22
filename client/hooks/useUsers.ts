d627ca99121b6a9d4bd9a9c2b89635df9fe3e271
ezemastro <mastropietroezequiel@gmail.com>
2025-09-21 23:10:22 -0300

    Agregar manejo de errores en la función fetchUsers para mejorar la gestión de errores de la API

--- client/hooks/useUsers.ts
+++ client/hooks/useUsers.ts
@@ -1,17 +1,25 @@
 import { api } from "@/api/loop";
+import { ApiError, parseErrorName } from "@/services/errors";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
-const fetchUsers = async (
-  params: GetUsersRequest["query"],
-): Promise<{ users: User[]; pagination: Pagination }> => {
-  const { page = 1, searchTerm = "" } = params || {};
-  const response = await api.get<GetUsersResponse>("/users", {
-    params: { page, searchTerm },
-  });
-  return {
-    users: response.data.data!.users,
-    pagination: response.data.pagination,
-  };
+const fetchUsers = async (params: GetUsersRequest["query"]) => {
+  try {
+    const { page = 1, searchTerm = "" } = params || {};
+    const response = await api.get<GetUsersResponse>("/users", {
+      params: { page, searchTerm },
+    });
+    return response.data;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      const errName = parseErrorName({ status: err.response?.status || 500 });
+      throw {
+        name: errName,
+        message: err.message,
+      } as ApiError;
+    }
+    throw err;
+  }
 };
 
 export const useUsers = (params?: GetUsersRequest["query"]) => {
