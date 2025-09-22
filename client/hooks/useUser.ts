a426412c6d5b2bc0759bba0d9a7a423bec23a644
ezemastro <mastropietroezequiel@gmail.com>
2025-09-21 23:10:16 -0300

    Agregar manejo de errores en la función fetchUser para mejorar la gestión de errores de la API

--- client/hooks/useUser.ts
+++ client/hooks/useUser.ts
@@ -1,11 +1,24 @@
 import { api } from "@/api/loop";
+import { ApiError, parseErrorName } from "@/services/errors";
 import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
 const fetchUser = async (params: GetUserByIdRequest["params"]) => {
-  const response = await api.get<GetUserByIdResponse>(
-    `/users/${params.userId}`,
-  );
-  return response.data.data;
+  try {
+    const response = await api.get<GetUserByIdResponse>(
+      `/users/${params.userId}`,
+    );
+    return response.data.data;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      const errName = parseErrorName({ status: err.response?.status || 500 });
+      throw {
+        name: errName,
+        message: err.message,
+      } as ApiError;
+    }
+    throw err;
+  }
 };
 
 export const useUser = (params: GetUserByIdRequest["params"]) => {
