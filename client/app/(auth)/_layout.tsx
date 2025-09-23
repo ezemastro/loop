a3362f5fba88b4e656e493c58cec0eb9f2387d52
ezemastro <mastropietroezequiel@gmail.com>
2025-09-23 14:45:05 -0300

    Actualizar AuthLayout: agregar opciones de estilo para la barra de pestañas y definir íconos para las pantallas de inicio de sesión y registro.

--- client/app/(auth)/_layout.tsx
+++ client/app/(auth)/_layout.tsx
@@ -1,15 +1,31 @@
+import { AddUserIcon, UserIcon } from "@/components/Icons";
+import { COLORS } from "@/config";
 import { Tabs } from "expo-router";
 
 export default function AuthLayout() {
   return (
-    <Tabs screenOptions={{ tabBarHideOnKeyboard: true }}>
+    <Tabs
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        tabBarInactiveTintColor: COLORS.SECONDARY_TEXT,
+        tabBarActiveTintColor: COLORS.PRIMARY,
+      }}
+    >
       <Tabs.Screen
         name="login"
-        options={{ title: "Iniciar sesión", headerShown: false }}
+        options={{
+          title: "Iniciar sesión",
+          headerShown: false,
+          tabBarIcon: ({ color }) => <UserIcon style={{ color }} />,
+        }}
       />
       <Tabs.Screen
         name="register"
-        options={{ title: "Registrarse", headerShown: false }}
+        options={{
+          title: "Registrarse",
+          headerShown: false,
+          tabBarIcon: ({ color }) => <AddUserIcon style={{ color }} />,
+        }}
       />
     </Tabs>
   );
