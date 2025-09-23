8db3a5f8d97ea93e7093ca483c64a3577bed115a
ezemastro <mastropietroezequiel@gmail.com>
2025-09-23 14:45:01 -0300

    Mejorar el componente School: ajustar el estilo de la imagen para usar 'resizeMode: contain'.

--- client/components/cards/School.tsx
+++ client/components/cards/School.tsx
@@ -4,7 +4,11 @@ export default function School({ school }: { school: School }) {
   return (
     <View className="p-1 flex-row bg-white rounded">
       <View>
-        <Image source={{ uri: school.media.url }} className="size-16" />
+        <Image
+          source={{ uri: school.media.url }}
+          className="size-16"
+          style={{ resizeMode: "contain" }}
+        />
       </View>
       <View className="flex-1 justify-center items-center">
         <Text className="text-xl">{school.name}</Text>
