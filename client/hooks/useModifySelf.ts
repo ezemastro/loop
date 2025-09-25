f69ee655f7684fbd935c78c797458ab19ffa6a11
ezemastro <mastropietroezequiel@gmail.com>
2025-09-25 13:11:40 -0300

    Agregar hook useModifySelf para manejar la modificación de datos del usuario

---
+++ client/hooks/useModifySelf.ts
@@ -0,0 +1,30 @@
+import { api } from "@/api/loop";
+import { ApiError, parseErrorName } from "@/services/errors";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+const fetchModifySelf = async (params: PatchSelfRequest["body"]) => {
+  try {
+    const response = await api.patch<PatchListingsResponse>(`/me`, params);
+
+    if (!response.data.success) {
+      throw new Error(response.data.error || "Error desconocido");
+    }
+    return response.data.data;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      const errName = parseErrorName({ status: err.response?.status || 500 });
+      throw {
+        name: errName,
+        message: err.message,
+      } as ApiError;
+    }
+    throw err;
+  }
+};
+
+export const useModifySelf = () => {
+  return useMutation({
+    mutationFn: fetchModifySelf,
+  });
+};
