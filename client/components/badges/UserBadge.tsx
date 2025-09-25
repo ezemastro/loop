2dd84289a0df6730898071b71812d453d331b5c4
ezemastro <mastropietroezequiel@gmail.com>
2025-09-25 13:11:29 -0300

    Refactorizar componente UserBadge para usar twMerge para el manejo de clases y agregar getUrl para obtener la url de la imagen

--- client/components/badges/UserBadge.tsx
+++ client/components/badges/UserBadge.tsx
@@ -1,5 +1,7 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
+import { getUrl } from "@/services/getUrl";
+import { twMerge } from "tailwind-merge";
 
 export default function UserBadge({
   user,
@@ -13,12 +15,14 @@ export default function UserBadge({
   containerClassName?: string;
 }) {
   return (
-    <View className={`flex-row items-center gap-2 ${containerClassName}`}>
+    <View
+      className={twMerge(`flex-row items-center gap-2`, containerClassName)}
+    >
       <Image
-        source={{ uri: user.profileMedia?.url }}
-        className={`size-6 rounded-full bg-background ${imageClassName}`}
+        source={{ uri: getUrl(user.profileMedia?.url || "") }}
+        className={twMerge(`size-6 rounded-full bg-background`, imageClassName)}
       />
-      <Text className={`text-sm text-secondary-text ${textClassName}`}>
+      <Text className={twMerge(`text-secondary-text text-sm`, textClassName)}>
         {user.firstName} {user.lastName}
       </Text>
     </View>
