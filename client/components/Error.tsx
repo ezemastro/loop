5d41be7232f20c47a425fd2a060e3c52c92ca520
ezemastro <mastropietroezequiel@gmail.com>
2025-09-25 13:13:17 -0300

    Mejorar el componente Error para utilizar twMerge en la combinación de clases y permitir la personalización del estilo del texto.

--- client/components/Error.tsx
+++ client/components/Error.tsx
@@ -1,15 +1,20 @@
 import { View, Text } from "react-native";
+import { twMerge } from "tailwind-merge";
 
 export default function Error({
   children,
   className,
+  textClassName,
 }: {
   children: React.ReactNode;
   className?: string;
+  textClassName?: string;
 }) {
   return (
-    <View className={`items-center justify-center ${className}`}>
-      <Text className="text-main-text">{children}</Text>
+    <View className={twMerge("items-center justify-center", className)}>
+      <Text className={twMerge("text-main-text", textClassName)}>
+        {children}
+      </Text>
     </View>
   );
 }
