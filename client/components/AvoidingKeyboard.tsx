347e1482e6988b420f9e3ffa8af656a1a9827829
ezemastro <mastropietroezequiel@gmail.com>
2025-09-24 21:40:28 -0300

    Refactor el componente AvoidingKeyboard para simplificar la lógica de renderizado y mejorar la gestión del teclado en iOS

--- client/components/AvoidingKeyboard.tsx
+++ client/components/AvoidingKeyboard.tsx
@@ -1,5 +1,5 @@
 import { useHideOnKeyboard } from "@/hooks/useHideOnKeyboard";
-import { Keyboard, KeyboardAvoidingView, Platform } from "react-native";
+import { Keyboard, KeyboardAvoidingView, Platform, View } from "react-native";
 
 export default function AvoidingKeyboard({
   children,
@@ -8,16 +8,25 @@ export default function AvoidingKeyboard({
 }) {
   const keyboardHeight = Keyboard.metrics()?.height;
   const { visible } = useHideOnKeyboard();
+  if (Platform.OS === "ios") {
+    return (
+      <KeyboardAvoidingView
+        behavior="padding"
+        className="flex-1"
+        keyboardVerticalOffset={170}
+      >
+        {children}
+      </KeyboardAvoidingView>
+    );
+  }
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    <View
       className="flex-1"
       style={{
-        paddingBottom: !visible && Platform.OS !== "ios" ? keyboardHeight : 0,
+        paddingBottom: !visible ? keyboardHeight : 0,
       }}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 170 : 0}
     >
       {children}
-    </KeyboardAvoidingView>
+    </View>
   );
 }
