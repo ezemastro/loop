d6f414e10fc50703c03cbeb69be738cf1f8f55c7
ezemastro <mastropietroezequiel@gmail.com>
2025-10-22 21:36:07 -0300

    Agregar z-index al botón de cerrar modal para mejorar la superposición

--- client/components/CloseModalButton.tsx
+++ client/components/CloseModalButton.tsx
@@ -4,7 +4,7 @@ import { CrossIcon } from "./Icons";
 
 export default function CloseModalButton({ onClose }: { onClose: () => void }) {
   return (
-    <Pressable onPress={onClose} className="p-2 absolute top-2 right-2">
+    <Pressable onPress={onClose} className="p-2 absolute top-2 right-2 z-10">
       <CrossIcon size={24} className="text-main-text" />
     </Pressable>
   );
