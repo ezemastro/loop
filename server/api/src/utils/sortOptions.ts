ee43ca2e0d9b8238d9a4d45668bd433c705ee6aa
ezemastro <mastropietroezequiel@gmail.com>
2025-10-25 10:39:31 -0300

    Mejorar la tipificación de SORT y ajustar las claves de categoría, vendedor y comprador en sortOptions.ts

--- server/api/src/utils/sortOptions.ts
+++ server/api/src/utils/sortOptions.ts
@@ -1,17 +1,17 @@
-export const SORT = {
+export const SORT: Record<SortOptions, string> = {
   createdAt: "created_at",
   price: "price_credits",
   title: "title",
   updatedAt: "updated_at",
   listingStatus: "listing_status",
-  category: "category_id",
-  seller: "seller_id",
-  buyer: "buyer_id",
+  categoryId: "category_id",
+  sellerId: "seller_id",
+  buyerId: "buyer_id",
 } as const;
 export const DEFAULT_SORT_OPTION = SORT.createdAt;
 export const DEFAULT_ORDER_OPTION = "desc";
 export const SORT_OPTIONS = Object.keys(SORT);
-export const getSortValue = (key?: keyof typeof SORT) => {
+export const getSortValue = (key?: SortOptions) => {
   if (!key) return DEFAULT_SORT_OPTION;
   return SORT[key] || DEFAULT_SORT_OPTION;
 };
