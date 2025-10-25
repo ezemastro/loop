8cf18a3c5fa6dbe88b17bb346f3a5a79c1891622
ezemastro <mastropietroezequiel@gmail.com>
2025-10-25 12:27:45 -0300

    Mejorar la validación del formulario de registro y ajustar los mensajes de error para una mejor experiencia de usuario.

--- client/services/validations.ts
+++ client/services/validations.ts
@@ -1,6 +1,7 @@
 import {
   MAX_LISTING_DESCRIPTION_LENGTH,
   MAX_LISTING_TITLE_LENGTH,
+  PRODUCT_STATUSES,
   VALID_EMAIL_DOMAINS,
 } from "@/config";
 import { z } from "zod";
@@ -18,36 +19,45 @@ const registerFormSchema = z.object({
     const domain = email.split("@")[1];
     return VALID_EMAIL_DOMAINS.includes(domain);
   }, "El correo electrónico debe pertenecer a Reditinere"),
-  password: z.string().min(6).max(100),
-  confirmPassword: z.string().min(6).max(100),
+  password: z
+    .string()
+    .min(6, "La contraseña es demasiado corta")
+    .max(100, "La contraseña es demasiado larga"),
+  confirmPassword: z.string(),
   schools: z
     .array(
       z.object({
         id: z.uuid(),
       }),
+      "Debes seleccionar al menos una escuela",
     )
-    .min(1),
-  role: z.object({
-    id: z.uuid(),
-  }),
+    .min(1, "Debes seleccionar al menos una escuela"),
 });
 export const validateRegisterForm = (data: unknown) => {
   return registerFormSchema.safeParse(data);
 };
 const loginFormSchema = z.object({
-  email: z.email(),
-  password: z.string().min(6).max(100),
+  email: z.email("El correo electrónico no es válido"),
+  password: z.string(),
 });
 export const validateLoginForm = (data: unknown) => {
   return loginFormSchema.safeParse(data);
 };
 
 const publishListingFormSchema = z.object({
-  title: z.string().min(2).max(MAX_LISTING_TITLE_LENGTH),
-  description: z.string().max(MAX_LISTING_DESCRIPTION_LENGTH).nullable(),
+  title: z
+    .string()
+    .min(2, "El título es demasiado corto")
+    .max(MAX_LISTING_TITLE_LENGTH, "El título es demasiado largo"),
+  description: z
+    .string()
+    .max(MAX_LISTING_DESCRIPTION_LENGTH, "La descripción es demasiado larga")
+    .nullable(),
   categoryId: z.uuid(),
-  productStatus: z.string().min(2).max(100),
-  price: z.number().min(0),
+  productStatus: z.string().refine((status) => {
+    return Object.values(PRODUCT_STATUSES).includes(status as ProductStatus);
+  }, "El estado del producto no es válido"),
+  price: z.number().min(1, "Introduce un precio válido"),
 });
 export const validatePublishListingForm = (data: unknown) => {
   return publishListingFormSchema.safeParseAsync(data);
