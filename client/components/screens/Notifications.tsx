d96a7ccfd0a86c107627b6077325612c3125c4e7
ezemastro <mastropietroezequiel@gmail.com>
2025-10-25 16:08:27 -0300

    Refactor componente Notifications para utilizar FlatList y mejorar la gestión de notificaciones

--- client/components/screens/Notifications.tsx
+++ client/components/screens/Notifications.tsx
@@ -1,15 +1,48 @@
-import { Text } from "react-native";
-import React from "react";
+import { FlatList, Text } from "react-native";
 import { MainView } from "../bases/MainView";
-import TextTitle from "../bases/TextTitle";
+import { useNotifications } from "@/hooks/useNotifications";
+import NotificationCard from "../cards/Notification";
+import Loader from "../Loader";
+import { useReadNotifications } from "@/hooks/useReadNotifications";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Notifications() {
+  const queryClient = useQueryClient();
+  const { data, isLoading, isError, fetchNextPage } = useNotifications();
+  const { mutateAsync: readAllNotifications } = useReadNotifications();
+  const notifications =
+    data?.pages.flatMap((page) => page.data!.notifications) || [];
+
+  if (!isLoading && notifications.length > 0) {
+    readAllNotifications().then(() => {
+      queryClient.invalidateQueries({ queryKey: ["unreadNotifications"] });
+    });
+  }
+
   return (
     <MainView className="p-4">
-      <TextTitle>Notificaciones</TextTitle>
-      <Text className="text-secondary-text text-center">
-        Aquí aparecerán tus notificaciones.
-      </Text>
+      <FlatList
+        data={notifications}
+        contentContainerClassName="gap-2"
+        renderItem={({ item }) => <NotificationCard notification={item} />}
+        onEndReached={() => fetchNextPage()}
+        onEndReachedThreshold={0.5}
+        ListEmptyComponent={() => (
+          <>
+            {isLoading && <Loader />}
+            {!isLoading && !isError && !notifications.length && (
+              <Text className="text-secondary-text text-center">
+                No tienes notificaciones
+              </Text>
+            )}
+            {isError && (
+              <Text className="text-red-500 text-center">
+                Error al cargar las notificaciones
+              </Text>
+            )}
+          </>
+        )}
+      />
     </MainView>
   );
 }
