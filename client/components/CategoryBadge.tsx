88d19d8fa768375bcd4f6add734a8ebf601caeba
ezemastro <mastropietroezequiel@gmail.com>
2025-10-29 19:45:01 -0300

    Agregar lógica para resaltar categorías deseadas en CategoryBadge

--- client/components/CategoryBadge.tsx
+++ client/components/CategoryBadge.tsx
@@ -1,15 +1,29 @@
+import { useWishes } from "@/hooks/useWishes";
 import { Text } from "react-native";
 import { twMerge } from "tailwind-merge";
 
 export default function CategoryBadge({
   category,
   className,
+  colorless,
 }: {
   category: Category;
   className?: string;
+  colorless?: boolean;
 }) {
+  const { data } = useWishes();
+  const wishedCategories = data?.userWishes || [];
+  const isWished = wishedCategories.some(
+    (wish) => wish.categoryId === category.id,
+  );
   return (
-    <Text className={twMerge("text-secondary-text", className)}>
+    <Text
+      className={twMerge(
+        "text-secondary-text",
+        isWished && colorless !== true && "text-secondary",
+        className,
+      )}
+    >
       {category.parents &&
         category.parents?.map((parent) => parent.name).join(" / ") + " / "}
       {category.name}
