d949a60490f844d35f6747cab584b9eed2467964
ezemastro <mastropietroezequiel@gmail.com>
2025-10-29 19:45:14 -0300

    Eliminar lógica de visualización de fechas en ChatDayLabel y simplificar el componente para usar solo DateBadge.

--- client/components/ChatDayLabel.tsx
+++ client/components/ChatDayLabel.tsx
@@ -1,26 +1,11 @@
-import { View, Text } from "react-native";
-const getShortDayName = (date: Date) => {
-  const days = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
-  return days[date.getDay()];
-};
+import { View } from "react-native";
+import { DateBadge } from "./bases/DateBadge";
 
 export default function ChatDayLabel({ date }: { date: Date }) {
   date = new Date(date);
-  const isToday = date.toDateString() === new Date().toDateString();
-  const isYesterday =
-    date.toDateString() === new Date(Date.now() - 86400000).toDateString();
-
-  let text: string;
-  if (isToday) {
-    text = "Hoy";
-  } else if (isYesterday) {
-    text = "Ayer";
-  } else {
-    text = getShortDayName(date) + ". " + date.toLocaleDateString();
-  }
   return (
     <View className="items-center p-2">
-      <Text className="text-secondary-text">{text}</Text>
+      <DateBadge date={date} />
     </View>
   );
 }
