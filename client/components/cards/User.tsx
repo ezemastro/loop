1b7c800fab49737a5652332467a516ba94d74f47
ezemastro <mastropietroezequiel@gmail.com>
2025-10-29 19:56:32 -0300

    Refactorizar componente para aceptar className y mejorar el manejo de la URL de la imagen

--- client/components/cards/User.tsx
+++ client/components/cards/User.tsx
@@ -1,19 +1,33 @@
+import { getUrl } from "@/services/getUrl";
 import { View, Text, Image } from "react-native";
+import { twMerge } from "tailwind-merge";
 
-export default function User({ user }: { user: PublicUser }) {
+export default function User({
+  user,
+  className,
+}: {
+  user: PublicUser;
+  className?: string;
+}) {
   return (
-    <View className="p-2 flex-row bg-white rounded">
+    <View className={twMerge("p-2 flex-row bg-white rounded", className)}>
       <Image
-        source={{ uri: user.profileMedia?.url }}
+        source={{ uri: getUrl(user.profileMedia?.url ?? "") }}
         className="size-16 rounded-full bg-background"
       />
       <View className="flex-1 justify-center items-center">
         <Text className="text-xl text-main-text">
           {user.firstName} {user.lastName}
         </Text>
-        <Text className="text-secondary-text ">
-          {user.schools.map((s) => s.name).join(", ")}
-        </Text>
+        <View className="flex-row">
+          {user.schools.map((s) => (
+            <Image
+              key={s.id}
+              source={{ uri: getUrl(s.media.url) }}
+              className="size-8 rounded-full bg-background mx-1"
+            />
+          ))}
+        </View>
       </View>
     </View>
   );
