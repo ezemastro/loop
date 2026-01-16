216cb78cbb44acc9658ae66c2b02d9f6bde4ae1d
ezemastro <mastropietroezequiel@gmail.com>
2026-01-15 22:14:18 -0300

    Agregar rutas para la gestión de usuarios, categorías, misiones y notificaciones en el enrutador principal.

--- adminClient/src/main.tsx
+++ adminClient/src/main.tsx
@@ -6,12 +6,20 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Register from "@/pages/Register.tsx";
 import Login from "@/pages/Login";
 import Schools from "@/pages/Schools";
+import Users from "@/pages/Users";
+import Categories from "@/pages/Categories";
+import Missions from "@/pages/Missions";
+import Notifications from "@/pages/Notifications";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/schools" element={<Schools />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/notifications" element={<Notifications />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/*" element={<Home />} />
