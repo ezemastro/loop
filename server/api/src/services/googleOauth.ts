67822f62ea94dd25c67d725d0dc972171e969d7f
ezemastro <mastropietroezequiel@gmail.com>
2026-02-28 14:26:06 +0000

    Refactor import statements in googleOauth.ts: eliminar líneas en blanco innecesarias

--- server/api/src/services/googleOauth.ts
+++ server/api/src/services/googleOauth.ts
@@ -1,9 +1,6 @@
 import { OAuth2Client } from "google-auth-library";
-import {
-  ADMIN_GOOGLE_CLIENT_ID,
-  WEB_GOOGLE_CLIENT_ID,
-} from "../config";
+import { ADMIN_GOOGLE_CLIENT_ID, WEB_GOOGLE_CLIENT_ID } from "../config";
 
 export const adminGoogleClient = new OAuth2Client(ADMIN_GOOGLE_CLIENT_ID);
 
-export const webGoogleClient= new OAuth2Client(WEB_GOOGLE_CLIENT_ID);
+export const webGoogleClient = new OAuth2Client(WEB_GOOGLE_CLIENT_ID);
