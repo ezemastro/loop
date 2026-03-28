f965f6edf61b59b100c83a254d363ff05bbf2702
ezemastro <mastropietroezequiel@gmail.com>
2026-03-28 20:41:20 +0000

    Actualizar configuración de Google Sign-In: agregar verificación de plataforma para evitar la configuración en web.

--- client/services/googleOauth.ts
+++ client/services/googleOauth.ts
@@ -1,8 +1,13 @@
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { Platform } from "react-native";
 import { IOS_GOOGLE_CLIENT_ID, WEB_GOOGLE_CLIENT_ID } from "../config";
 
 // Configurar Google Sign-In (ejecutar al inicio de la app)
 export const configureGoogleSignIn = () => {
+  if (Platform.OS === "web") {
+    return;
+  }
+
   GoogleSignin.configure({
     webClientId: WEB_GOOGLE_CLIENT_ID,
     iosClientId: IOS_GOOGLE_CLIENT_ID,
