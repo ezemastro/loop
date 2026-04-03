0450c9751fcab8373202fb07700c2a4e375c1c1c
ezemastro <mastropietroezequiel@gmail.com>
2026-04-03 14:58:15 +0000

    Actualizar componente AppLogo: modificar la estructura del logo para incluir un contenedor con fondo y redondeado, ajustando el tamaño de la imagen.

--- client/components/header/AppLogo.tsx
+++ client/components/header/AppLogo.tsx
@@ -3,12 +3,13 @@ import { View, Text, Image } from "react-native";
 export default function AppLogo() {
   return (
     <View className="flex-row items-center gap-2">
-      <Image
-        source={require("../../assets/icon.png")}
-        className="bg-white rounded-full"
-        style={{ width: 32, height: 32 }}
-        resizeMode="contain"
-      />
+      <View className="w-12 h-12 bg-white/90 rounded-full p-0.5">
+        <Image
+          source={require("../../assets/icon.png")}
+          style={{ width: "100%", height: "100%" }}
+          resizeMode="contain"
+        />
+      </View>
       <Text className="text-white font-semibold tracking-wider text-xl">
         Loop
       </Text>
