d57a518464130bbbdeb0fe09b370c6819bb3257b
ezemastro <mastropietroezequiel@gmail.com>
2026-04-11 15:53:11 +0000

    Agregar componente MainView en Landing para mejorar la estructura del diseño.

--- client/components/screens/Landing.tsx
+++ client/components/screens/Landing.tsx
@@ -1,5 +1,6 @@
 import ButtonText from "@/components/bases/ButtonText";
 import CustomButton from "@/components/bases/CustomButton";
+import { MainView } from "@/components/bases/MainView";
 import { GoogleSignInButton } from "@/components/buttons/GoogleSignInButton";
 import { useRouter } from "expo-router";
 import { View, Text, Image, ScrollView } from "react-native";
@@ -7,45 +8,47 @@ import { View, Text, Image, ScrollView } from "react-native";
 export default function Landing() {
   const router = useRouter();
   return (
-    <ScrollView className="flex-1">
-      <View className="py-16 px-6 flex-1 h-screen-safe">
-        <Image
-          source={require("../../assets/reditinere_logo.png")}
-          className="mx-auto"
-          style={{ width: 160, height: 128 }}
-          resizeMode="contain"
-        />
-        <Image
-          source={require("../../assets/full_logo.png")}
-          className="mx-auto"
-          style={{ width: 320, height: 144 }}
-          resizeMode="contain"
-        />
-        <Text className="text-center text-main-text font-bold text-xl mt-4 w-80 mx-auto">
-          Una app donde podrás darle una segunda vida a tus útiles usados.
-        </Text>
-        <Text className="text-main-text text-lg mx-auto text-center my-8">
-          ¡Únete a nuestra comunidad y descubre cómo puedes contribuir a un
-          mundo más sostenible mientras encuentras tesoros ocultos en la
-          comunidad!
-        </Text>
-        <Text className="text-main-text w-96 mx-auto text-center my-2 pt-2 mt-auto">
-          Accede con un solo clic usando tu cuenta de Google
-        </Text>
-        <GoogleSignInButton />
-        <Text className="text-main-text w-96 mx-auto text-center my-2 mt-4">
-          ¿Primera vez aquí? ¡Regístrate ahora!
-        </Text>
-        <CustomButton onPress={() => router.push("/register")}>
-          <ButtonText>Registrarse</ButtonText>
-        </CustomButton>
-        <Text className="text-main-text w-96 mx-auto text-center my-2 mt-4">
-          ¿Ya tienes una cuenta? ¡Inicia sesión ahora!
-        </Text>
-        <CustomButton onPress={() => router.push("/login")}>
-          <ButtonText>Iniciar Sesión</ButtonText>
-        </CustomButton>
-      </View>
-    </ScrollView>
+    <MainView>
+      <ScrollView className="flex-1">
+        <View className="py-16 px-6 flex-1 h-screen-safe">
+          <Image
+            source={require("../../assets/reditinere_logo.png")}
+            className="mx-auto"
+            style={{ width: 160, height: 128 }}
+            resizeMode="contain"
+          />
+          <Image
+            source={require("../../assets/full_logo.png")}
+            className="mx-auto"
+            style={{ width: 320, height: 144 }}
+            resizeMode="contain"
+          />
+          <Text className="text-center text-main-text font-bold text-xl mt-4 w-80 mx-auto">
+            Una app donde podrás darle una segunda vida a tus útiles usados.
+          </Text>
+          <Text className="text-main-text text-lg mx-auto text-center my-8">
+            ¡Únete a nuestra comunidad y descubre cómo puedes contribuir a un
+            mundo más sostenible mientras encuentras tesoros ocultos en la
+            comunidad!
+          </Text>
+          <Text className="text-main-text w-96 mx-auto text-center my-2 pt-2 mt-auto">
+            Accede con un solo clic usando tu cuenta de Google
+          </Text>
+          <GoogleSignInButton />
+          <Text className="text-main-text w-96 mx-auto text-center my-2 mt-4">
+            ¿Primera vez aquí? ¡Regístrate ahora!
+          </Text>
+          <CustomButton onPress={() => router.push("/register")}>
+            <ButtonText>Registrarse</ButtonText>
+          </CustomButton>
+          <Text className="text-main-text w-96 mx-auto text-center my-2 mt-4">
+            ¿Ya tienes una cuenta? ¡Inicia sesión ahora!
+          </Text>
+          <CustomButton onPress={() => router.push("/login")}>
+            <ButtonText>Iniciar Sesión</ButtonText>
+          </CustomButton>
+        </View>
+      </ScrollView>
+    </MainView>
   );
 }
